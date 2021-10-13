40ae67f29f0494959ebc094d980c94152e02c463
pipe028 <pipebermudez28@gmail.com>
2021-10-12 23:41:02 -0500

    U

--- Dominio/Entidades/Arbitro.cs
+++ Dominio/Entidades/Arbitro.cs
@@ -14,10 +14,6 @@ namespace Dominio
         //llave foranea
         public int TorneoId{get;set;}
         public int EscuelaArbitroId{get;set;}
-<<<<<<< HEAD
-        //Hello
-=======
-        //ddddd
->>>>>>> 20cfbb016738b281c059ddb961bc5d13e34c2731
+
     }
 }
